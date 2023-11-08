51afca8057417ce8411f0f9f2b5d39f4664f5cc7
lapishi <lapish1214@gmail.com>
2023-11-08 09:01:02 +0900

    리팩토링

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -5,13 +5,19 @@ import java.util.List;
 import java.util.Scanner;
 
 public class App {
+    Scanner scanner;
+    int setNumber;
+    List<SetNumber3> setNumberArray;
+
+    App() {
+        scanner = new Scanner(System.in);
+        setNumber = 0;
+        setNumberArray = new ArrayList<>();
+    }
 
     void run() {
         System.out.println("== 명언 앱 ==");
-        Scanner scanner = new Scanner(System.in);
 
-        int setNumber = 0;
-        List<SetNumber3> setNumberArray = new ArrayList<>();
 
         while (true) {
             System.out.print("명령) ");
@@ -20,32 +26,45 @@ public class App {
             if (line.equals("종료")) {
                 break;
             } else if (line.equals("등록")) {
-                System.out.print("명언 : ");
-                String content = scanner.nextLine();
-
-                System.out.print("작가 : ");
-                String author = scanner.nextLine();
-
-                setNumber++;
-                int id = setNumber;
-
-                SetNumber3 setNumber3 = new SetNumber3(id, content, author);
-                setNumberArray.add(setNumber3);
-
-                System.out.printf("%d번 명언이 등록되었습니다.\n", setNumber);
-
+                actionWrite();
             } else if (line.equals("목록")) {
-                System.out.println("번호 / 작가 / 명언" + "\n ------------------");
-
-                if (setNumberArray.isEmpty())
-                    System.out.println("등록된 명언이 없습니다.");
-
-                for (int i = setNumberArray.size() - 1; i >= 0; i--) {
-                    SetNumber3 setNumber3 = setNumberArray.get(i);
-                    System.out.printf("%d / %s / %s\n", setNumber3.id, setNumber3.author, setNumber3.content);
-                }
+                actionList();
             }
         }
     }
-}
 
+
+    void actionWrite() {
+        System.out.print("명언 : ");
+        String content = scanner.nextLine();
+
+        System.out.print("작가 : ");
+        String author = scanner.nextLine();
+
+        setNumber++;
+        int id = setNumber;
+
+        SetNumber3 setNumber3 = new SetNumber3(id, content, author);
+        setNumberArray.add(setNumber3);
+
+        System.out.printf("%d번 명언이 등록되었습니다.\n", setNumber);
+
+
+    }
+
+    void actionList() {
+        System.out.println("번호 / 작가 / 명언" + "\n ------------------");
+
+        if (setNumberArray.isEmpty())
+            System.out.println("등록된 명언이 없습니다.");
+
+        for (int i = setNumberArray.size() - 1; i >= 0; i--) {
+            SetNumber3 setNumber3 = setNumberArray.get(i);
+            System.out.printf("%d / %s / %s\n", setNumber3.id, setNumber3.author, setNumber3.content);
+
+        }
+
+
+    }
+
+}
