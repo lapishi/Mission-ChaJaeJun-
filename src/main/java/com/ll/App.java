75906dc31b221704f290828588100ed92ca2ca1e
lapishi <lapish1214@gmail.com>
2023-11-08 14:25:10 +0900

    getParamAsInt 빠진 것 추가, 커밋 꼬인듯?

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -25,19 +25,20 @@ public class App {
 
             Rq rq = new Rq(line);
 
-
-            if (line.equals("종료")) {
+            if (rq.getAction().equals("종료")) {
                 break;
-            } else if (line.equals("등록")) {
+            } else if (rq.getAction().equals("등록")) {
                 actionWrite();
-            } else if (line.equals("목록")) {
+            } else if (rq.getAction().equals("목록")) {
                 actionList();
-            } else if (line.startsWith("삭제?")) {
+            } else if (rq.getAction().equals("삭제")) {
                 actionRemove(line);
+            } else if (rq.getAction().equals("수정")) {
+                actionModify(line);
             }
+
         }
     }
-
 
     void actionWrite() {
         System.out.print("명언 : ");
@@ -60,38 +61,57 @@ public class App {
     void actionList() {
         System.out.println("번호 / 작가 / 명언" + "\n ------------------");
 
-        if (setNumberArray.isEmpty())
-            System.out.println("등록된 명언이 없습니다.");
+        if (setNumberArray.isEmpty()) System.out.println("등록된 명언이 없습니다.");
 
         for (int i = setNumberArray.size() - 1; i >= 0; i--) {
             SetNumber3 setNumber3 = setNumberArray.get(i);
             System.out.printf("%d / %s / %s\n", setNumber3.id, setNumber3.author, setNumber3.content);
 
         }
-
-
     }
 
     void actionRemove(String line) {
-        String[] lineBits = line.split("\\?", 2);
-        String action = lineBits[0];
-        String queryString = lineBits[1];
+        int id = getParamAsInt(line, "id", 0);
+
+        if (id == 0) {
+            System.out.println("id를 정확히 입력해주세요");
+            return;
+        }
+        System.out.printf("%d번 명언을 삭제합니다.\n", id);
+    }
+
+    void actionModify(String line) {
+        int id = getParamAsInt(line, "id", 0);
+
+        if (id == 0) {
+            System.out.println("id를 정확히 입력해주세요.");
+            return;
+        }
+        System.out.printf("%d번 명언을 수정합니다.\n", id);
+    }
+
+    int getParamAsInt(String line, String paramName, int defaultValue) {//해당 메서드는 가져다 씀
+        String[] cmdBits = line.split("\\?", 2);
+        String queryString = cmdBits[1];
 
         String[] queryStringBits = queryString.split("&");
 
-        int id = 0;
-
         for (int i = 0; i < queryStringBits.length; i++) {
             String queryParamStr = queryStringBits[i];
+
             String[] queryParamStrBits = queryParamStr.split("=", 2);
-            String paramName = queryParamStrBits[0];
+
+            String _paramName = queryParamStrBits[0];
             String paramValue = queryParamStrBits[1];
 
-            if(paramName.equals("id")){
-                id = Integer.parseInt(paramValue);
+            if (_paramName.equals(paramName)) {
+                try {
+                    return Integer.parseInt(paramValue);
+                } catch (NumberFormatException e) {
+                    return defaultValue;
+                }
             }
         }
-
-        System.out.printf("%d번 명언을 삭제합니다.\n", id);
+        return defaultValue;
     }
 }
